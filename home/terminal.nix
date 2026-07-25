a5ceef252bed3d7f5ab90e0a8963e40969633f1f
Jason <jase100k@protonmail.com>
2026-07-25 20:17:12 +1000

    style: configure Alacritty with Catppuccin Mocha theme & JetBrains Mono font

--- home/terminal.nix
+++ home/terminal.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 
 {
+  # Kitty Terminal Configuration
   programs.kitty = {
     enable = true;
     themeFile = "Catppuccin-Mocha";
@@ -16,6 +17,67 @@
       cursor_shape = "beam";
       cursor_blink_interval = 0.5;
       hide_window_decorations = "yes";
+    };
+  };
+
+  # Alacritty Terminal Configuration (Catppuccin Mocha)
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        padding = {
+          x = 12;
+          y = 12;
+        };
+        opacity = 0.88;
+        blur = true;
+        decorations = "None";
+      };
+      font = {
+        normal = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Italic";
+        };
+        size = 11.0;
+      };
+      colors = {
+        primary = {
+          background = "#1e1e2e";
+          foreground = "#cdd6f4";
+        };
+        cursor = {
+          text = "#1e1e2e";
+          cursor = "#f5e0dc";
+        };
+        normal = {
+          black = "#45475a";
+          red = "#f38ba8";
+          green = "#a6e3a1";
+          yellow = "#f9e2af";
+          blue = "#89b4fa";
+          magenta = "#f5c2e7";
+          cyan = "#94e2d5";
+          white = "#bac2de";
+        };
+        bright = {
+          black = "#585b70";
+          red = "#f38ba8";
+          green = "#a6e3a1";
+          yellow = "#f9e2af";
+          blue = "#89b4fa";
+          magenta = "#f5c2e7";
+          cyan = "#94e2d5";
+          white = "#a6adc8";
+        };
+      };
     };
   };
 }
