5e866f4ed92264666b73667fb2cc5c10145ecfbb
Jason <jase100k@protonmail.com>
2026-07-25 23:31:16 +1000

    fix(shell): replace deprecated initExtra with initContent in zsh

--- home/shell.nix
+++ home/shell.nix
@@ -10,7 +10,7 @@
     autosuggestion.enable = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
-    initExtra = ''
+    initContent = ''
       eval "$(starship init zsh)"
     '';
 
